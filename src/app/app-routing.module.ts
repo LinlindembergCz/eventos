e1c04245a9dbdb85b36656e2657c27cf6a6321cf
STEFANINI-DOM\lcaraujo1 <lcaraujo1@latam.stefanini.com>
2022-08-27 18:23:17 -0300

    Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,60 @@ import { Routes, RouterModule, ExtraOptions, PreloadAllModules } from '@angular/
 import { AuthGuard } from './@bootstrap/security/auth.guard';
 import { BlankComponent } from './@layout/blank/blank.component';
 import { LayoutComponent } from './@layout/layout.component';
+import { ContatoComponent } from './@modules/contato/contatocomponent';
+import { EBookComponent } from './@modules/conteudo/ebook/ebook.component';
+import { EditaisComponent } from './@modules/conteudo/editais/editais.component';
+import { NoticiasComponent } from './@modules/conteudo/noticias/noticias.component';
+import { OutrosComponent } from './@modules/conteudo/outros/outros.component';
+import { EventosComponent } from './@modules/eventos/eventos.component';
+import { QuemSomosComponent } from './@modules/quemsomos/quemsomos.component';
 
 const routes: Routes = [
    {
-     path: 'home',
+     path: '',
      component: LayoutComponent,
      loadChildren: () => import('./@modules/home/home.module').then(m => m.HomeModule)
    },
   {
       path: '',
-      redirectTo: 'home/index',
+      redirectTo: '',
       pathMatch: 'full'
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent,
+    loadChildren: () => import('./@modules/contato/contato.module').then(m => m.ContatoModule)
+  }
+  ,
+  {
+    path: 'eventos',
+    component: EventosComponent,
+    loadChildren: () => import('./@modules/eventos/eventos.module').then(m => m.EventosModule)
+  },
+  {
+    path: 'ebook',
+    component: EBookComponent,
+    loadChildren: () => import('./@modules/conteudo/ebook/ebook.module').then(m => m.EBookModule)
+  },
+  {
+    path: 'editais',
+    component: EditaisComponent,
+    loadChildren: () => import('./@modules/conteudo/editais/editais.module').then(m => m.EditaisModule)
+  },
+  {
+    path: 'noticias',
+    component: NoticiasComponent,
+    loadChildren: () => import('./@modules/conteudo/noticias/noticias.module').then(m => m.NoticiasModule)
+  }, 
+  {
+    path: 'outros',
+    component: OutrosComponent,
+    loadChildren: () => import('./@modules/conteudo/outros/outros.module').then(m => m.OutrosModule)
+  },
+  {
+    path: 'quemsomos',
+    component: QuemSomosComponent,
+    loadChildren: () => import('./@modules/quemsomos/quemsomos.module').then(m => m.QuemSomosModule)
   },
   {
     path: 'user',
@@ -22,6 +65,7 @@ const routes: Routes = [
   }
 
 ];
+
 
 const config: ExtraOptions = {
   useHash: false,
